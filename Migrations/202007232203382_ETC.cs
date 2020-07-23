1532928c21aa6154a5df864c967692965cc01a9d
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2020-07-24 01:08:45 +0300

    Etc

---
+++ Migrations/202007232203382_ETC.cs
@@ -0,0 +1,16 @@
+namespace PhoneBook.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ETC : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
