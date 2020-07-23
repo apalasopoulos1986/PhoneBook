83ac3a4bfb9b27ac3ead2835725f06ffe42b1544
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2020-07-23 22:24:56 +0300

    FinishingCrudOperations

--- Controllers/ContactsAPIController.cs
+++ Controllers/ContactsAPIController.cs
@@ -17,12 +17,6 @@ namespace PhoneBook.Controllers
     {
         private MyDatabase db = new MyDatabase();
 
-        // GET: api/ContactsAPI
-        //public IQueryable<Contact> GetContacts()
-        //{
-        //    return db.Contacts;
-        //}
-
         public IHttpActionResult GetContacts()
         {
             var AllContacts = db.Contacts.ToList();
@@ -47,84 +41,7 @@ namespace PhoneBook.Controllers
             return Ok(AllContactsWithLocation);
         }
 
-        // GET: api/ContactsAPI/5
-        [ResponseType(typeof(Contact))]
-        public IHttpActionResult GetContact(int id)
-        {
-            Contact contact = db.Contacts.Find(id);
-            if (contact == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(contact);
-        }
-
-        // PUT: api/ContactsAPI/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutContact(int id, Contact contact)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != contact.ContactId)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(contact).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ContactExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/ContactsAPI
-        [ResponseType(typeof(Contact))]
-        public IHttpActionResult PostContact(Contact contact)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Contacts.Add(contact);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = contact.ContactId }, contact);
-        }
-
-        // DELETE: api/ContactsAPI/5
-        [ResponseType(typeof(Contact))]
-        public IHttpActionResult DeleteContact(int id)
-        {
-            Contact contact = db.Contacts.Find(id);
-            if (contact == null)
-            {
-                return NotFound();
-            }
-
-            db.Contacts.Remove(contact);
-            db.SaveChanges();
-
-            return Ok(contact);
-        }
+       
 
         protected override void Dispose(bool disposing)
         {
